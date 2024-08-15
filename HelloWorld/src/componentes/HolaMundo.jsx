df8cf121d1eba93045fa96cf87aae486d2917f80
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-15 02:32:01 +0000

    Hemos creado dos componentes desde cero

---
+++ HelloWorld/src/componentes/HolaMundo.jsx
@@ -0,0 +1,7 @@
+
+function HolaMundo (){
+
+    return <h1>DEW + React + Codespace - Poder Total</h1>
+}
+
+export default HolaMundo;
---
+++ HelloWorld/src/componentes/Mensaje.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+function Mensaje() {
+
+    return (
+        <>
+            <div>
+                <h1>Programando con React</h1>
+                <h2>Jesús Canales</h2>
+            </div>
+        </>
+    );
+}
+
+export default Mensaje;
