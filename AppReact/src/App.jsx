fb6fda07148e866bec814377c52a1be6eb74642b
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-17 00:50:15 +0000

    He creado una carpeta image y he subido una imagen que es el logo de cibertec

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -15,8 +15,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Cibertec</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
