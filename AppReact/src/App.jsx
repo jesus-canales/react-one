adee1dd21e10c40e1c065104713287a9e8cf497e
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-17 01:15:58 +0000

    He creado un componente de cacebera y lo he agregado a App.jsx

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -1,39 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ciberteclogo from './image/ciberteclogo.png'
-import './App.css'
+import { Fragment } from "react";
+import Menu from './componente/Menu'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.cibertec.edu.pe/" target="_blank">
-          <img src={ciberteclogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Cibertec</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Menu />
+    <Menu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ AppReact/src/componente/Menu.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function Menu(){
+
+    return (
+        <>
+        <h1>TravelFAST</h1>
+        <h3>Somos expertos en mostrarte lo mejor del Perú</h3>
+        </>
+
+    );
+}
+
+export default Menu;
