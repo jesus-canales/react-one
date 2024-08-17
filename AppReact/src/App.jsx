a673e4b12640a843ef50993db36ca84774044bf3
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-17 01:57:25 +0000

    He creado el componente ciudad y lo estoy utilizando desde el componente App.jsx

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -1,13 +1,17 @@
 import { Fragment } from "react";
 import Menu from './componente/Menu'
 import Pie from './componente/Pie'
+import Ciudad from "./componente/Ciudad";
 
 function App() {
 
   return (
     <>
       <Menu />
-         
+      <Ciudad
+      nombre="Lima"
+      descripcion="Capital del Perú"
+      imagen= "https://www.peru.travel/Contenido/General/Imagen/es/643/1.1/lima-plaza-de-armas-consejos.jpg" />
       <Pie />
     </>
   );
--- AppReact/src/componente/Ciudad.jsx
+++ AppReact/src/componente/Ciudad.jsx
@@ -1,10 +1,17 @@
 import { Fragment } from "react";
+import './estilosCiudad.css'
 
-function Ciudad () {
+function Ciudad({ nombre, descripcion, imagen }) {
 
-    return(
+    return (
         <>
-        
+            <div className="ciudad">
+                <h3 className="ciudad__titulo">{nombre}</h3>
+                <p className="ciudad__descripcion">{descripcion}</p>
+                <img src={imagen} alt={nombre} className="ciudad__imagen" />
+            </div>
         </>
     );
 }
+
+export default Ciudad;
