efbddfe3b4f1594020c34b9ea255c65fe8ac7e54
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-17 02:00:19 +0000

    He reutilizado el componente ciudad pasándole los parámetros con los datos de otra ciudad

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -12,6 +12,10 @@ function App() {
       nombre="Lima"
       descripcion="Capital del Perú"
       imagen= "https://www.peru.travel/Contenido/General/Imagen/es/643/1.1/lima-plaza-de-armas-consejos.jpg" />
+      <Ciudad
+      nombre="Arequipa"
+      descripcion="Encantadora tierra volcánica"
+      imagen = "https://www.peru.travel/contenido/destino/imagen/es/3/1.2/principal/plaza%20de%20armas%20arequipa.jpg" />
       <Pie />
     </>
   );
