822006bb4d26fa5d30039440ffb7d2052c5a6d7a
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-21 01:56:19 +0000

    Hemos implementado un acordion desde react bootstrap

--- AppWeb/src/App.jsx
+++ AppWeb/src/App.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Contador from './componentes/Contador'
+import Acordion from "./componentes/Acordion";
 
 function App(){
   return(
     <>
     <Contador />
+    <Acordion />
     </>
   );
 }
