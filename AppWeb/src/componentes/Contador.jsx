5d4c7ab79971bdd13a6c47731ee1cb6558b260f6
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-21 01:51:26 +0000

    Hemos agregado bootstrap en el componente para una mejor vista

--- AppWeb/src/componentes/Contador.jsx
+++ AppWeb/src/componentes/Contador.jsx
@@ -1,15 +1,23 @@
 import { Fragment, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Contador() {
-    const [ contar, setContar ] = useState(0);
+    const [contar, setContar] = useState(0);
     const hacerClic = () => {
-        setContar( contar + 1 );
+        setContar(contar + 1);
     }
 
     return (
         <>
-            <p>El número de clic realizados es: { contar }</p>
-            <button onClick={hacerClic}>Click aquí</button>
+            <div className="container mt-3">
+                <div className="row justify-content-center">
+                    <div className="col-md-auto">
+                        <p className="display-6 text-center">El número de clic realizados es: {contar}</p>
+                        <button type="button" className="btn btn-success" onClick={hacerClic}>Click aquí</button>
+                    </div>
+
+                </div>
+            </div>
         </>
     );
 }
