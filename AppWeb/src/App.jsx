d409a2e5b213bbcc5b9f50f150c02c26b7c34757
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-21 02:02:57 +0000

    Hemos implementado más elementos utilizando react bootstrap

--- AppWeb/src/App.jsx
+++ AppWeb/src/App.jsx
@@ -1,11 +1,15 @@
 import { Fragment } from "react";
 import Contador from './componentes/Contador'
 import Acordion from "./componentes/Acordion";
+import Carrusel from './componentes/Carrusel';
+import Formulario from './componentes/Formulario'
 
 function App(){
   return(
     <>
+    <Carrusel />
     <Contador />
+    <Formulario />
     <Acordion />
     </>
   );
---
+++ AppWeb/src/componentes/Carrusel.jsx
@@ -0,0 +1,45 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function DarkVariantExample() {
+  return (
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static.vecteezy.com/system/resources/thumbnails/024/692/112/small_2x/ai-generated-ai-generative-beautiful-rhododendron-flowers-over-sunset-mountains-field-landscape-graphic-art-photo.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static.vecteezy.com/system/resources/thumbnails/030/505/081/small_2x/alaska-mountain-range-wilderness-nature-landscape-snowy-mountains-wallpaper-ai-generated-photo.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://static.vecteezy.com/system/resources/thumbnails/029/346/830/small_2x/ai-generated-ai-generative-traditional-history-asian-japanese-chinese-temple-building-tower-garden-nature-outdoor-background-graphic-art-photo.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default DarkVariantExample;
