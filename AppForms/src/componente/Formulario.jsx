94928cf1fe8f149ba3450bd150956753ff3e73fd
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-23 01:11:42 +0000

    He creado un componente llamado Formulario

---
+++ AppForms/src/componente/Formulario.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Formulario = () => {
+    return (
+        <>
+        <h1>Hola soy el formulario</h1>
+        </>
+    );
+}
+
+export default Formulario;
