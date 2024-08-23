1d580a1e69313a53bb84cb18e9de4f1a5ffaacfc
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-23 02:11:56 +0000

    Hemos implementado expresiones regulares para exigir que se ingrese correctamente los datos

--- AppForms/src/componente/Formulario.jsx
+++ AppForms/src/componente/Formulario.jsx
@@ -8,6 +8,9 @@ const Formulario = () => {
     const [ lugar, setLugar ] = useState('');
     const [ errors, setErrors] = useState({});
 
+    const regexTexto = /^[a-zA-ZÁÉÍÓÚñ\s]+$/;
+    const regexNumero = /^[0-9]{1,9}$/;
+
     const realizarEnvio = (e) => {
         e.preventDefault();
         const newErrors = {};
@@ -15,6 +18,10 @@ const Formulario = () => {
         if(!apellidos) newErrors.apellidos = 'Los apellidos son obligatorio';
         if(!celular) newErrors.celular = 'El celular es obligatorio';
         if(!lugar) newErrors.lugar = 'El Lugar es obligatorio';
+        if(!regexTexto.test(nombre)) {alert('El nombre no es válido')};
+        if(!regexTexto.test(apellidos)) {alert('Los apellidos no son válidos')};
+        if(!regexNumero.test(celular)) {alert('Celular no válido')};
+        if(!regexTexto.test(lugar)) {alert('El lugar ingresado no es válido')};
         setErrors(newErrors);
         alert(`Datos ingresados: { ${nombre}, ${apellidos}, ${celular}, ${lugar} }`);
     }
