19fa3639200aae07363124e5f1e649863ec6fb1a
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-23 01:18:53 +0000

    Hemos creado la estructura del formulario

--- AppForms/src/componente/Formulario.jsx
+++ AppForms/src/componente/Formulario.jsx
@@ -1,9 +1,19 @@
 import { Fragment } from "react";
+import { Button, Form} from 'react-bootstrap';
 
 const Formulario = () => {
     return (
         <>
-        <h1>Hola soy el formulario</h1>
+        <Form>
+            <Form.Group>
+                <Form.Label>Formulario de Datos</Form.Label>
+                <Form.Control type="text" placeholder="Ingrese nombre"></Form.Control>
+                <Form.Control type="text" placeholder="Ingrese apellidos"></Form.Control>
+                <Form.Control type="text" placeholder="Ingrese celular"></Form.Control>
+                <Form.Control type="text" placeholder="Ingrese lugar de procedencia"></Form.Control>
+            </Form.Group>
+            <Button>Enviar datos</Button>
+        </Form>
         </>
     );
 }
